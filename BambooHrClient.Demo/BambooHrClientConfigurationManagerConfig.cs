c75ced7e026773a941b959c2236223d2def6ddda
Erik Karlsson <erik.karlsson@keg.com>
2023-07-21 15:36:57 +0200

    Added config implementation for demo project

---
+++ BambooHrClient.Demo/BambooHrClientConfigurationManagerConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+
+namespace BambooHrClient
+{
+    public class BambooHrClientConfigurationManagerConfig : IBambooHrClientConfig
+    {
+        public static readonly BambooHrClientConfigurationManagerConfig Instance = new();
+
+        public string BambooApiUser { get { return ConfigurationManager.AppSettings["BambooApiUser"]; } }
+        public string BambooApiKey { get { return ConfigurationManager.AppSettings["BambooApiKey"]; } }
+        public string BambooApiUrl { get { return ConfigurationManager.AppSettings["BambooApiUrl"]; } }
+        public string BambooCompanyUrl { get { return ConfigurationManager.AppSettings["BambooCompanyUrl"]; } }
+    }
+}
